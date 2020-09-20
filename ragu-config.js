8f6417eb31972508ccd51aa6442bf29bbf01a73d
Maniero <cmaniero@thoughtworks.com>
2020-09-20 17:32:13 -0300

    bumb ragu-server version

--- ragu-config.js
+++ ragu-config.js
@@ -30,5 +30,29 @@ module.exports = {
   components: {
     namePrefix: 'catalog',
     sourceRoot: path.join(__dirname, 'components'),
+    defaultDependencies: [
+      {
+        nodeRequire: 'react',
+        globalVariable: 'React',
+        dependency: 'https://unpkg.com/react@16.13.1/umd/react.production.min.js'
+      },
+      {
+        nodeRequire: 'react-is',
+        globalVariable: 'reactIs',
+        dependency: 'https://unpkg.com/react-is@16.13.1/umd/react-is.production.min.js'
+      },
+      {
+        nodeRequire: 'react-dom',
+        globalVariable: 'ReactDOM',
+        dependency: 'https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js',
+        order: 1
+      },
+      {
+        nodeRequire: 'styled-components',
+        globalVariable: 'styled',
+        dependency: 'https://unpkg.com/styled-components@5.2.0/dist/styled-components.min.js',
+        order: 1
+      }
+    ]
   },
 };
